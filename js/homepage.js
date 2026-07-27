19c4b2775185954a602d1b083800b74e00719e2e
sakina <sakinazehrapk@gmail.com>
2026-07-28 03:10:44 +0500

    added hompepage 1998

---
+++ js/homepage.js
@@ -0,0 +1,7 @@
+const counter = document.getElementById("counter");
+let visits = 129876;
+setInterval(() => {
+    visits++;
+    counter.textContent =
+        visits.toString().padStart(8,"0");
+},2000);

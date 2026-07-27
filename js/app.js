a83e1a4e2490639adcf7462bdce0c2b097fc09cb
sakina <sakinazehrapk@gmail.com>
2026-07-28 01:25:44 +0500

    added basic 1998 windows desktop

---
+++ js/app.js
@@ -0,0 +1,10 @@
+function updateClock(){
+    const clock=document.getElementById("clock");
+    const now=new Date();
+    clock.textContent=now.toLocaleTimeString([],{
+        hour:"2-digit",
+        minute:"2-digit"
+    });
+}
+updateClock();
+setInterval(updateClock,1000);
